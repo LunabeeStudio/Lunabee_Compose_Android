b70d9457ef26704b0c0fa4356e157adc3e744272
Hugo Bernardi <hugo.bernardi@lunabee.com>
2022-12-02 09:22:33 +0100

    Bump all versions for next release

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -31,9 +31,9 @@ object AndroidConfig {
     const val LIBRARY_URL: String = "https://github.com/LunabeeStudio/Lunabee_Compose_Android"
     const val GROUP_ID: String = "studio.lunabee.compose"
 
-    const val LBC_CORE_VERSION: String = "1.0.0"
-    const val LBC_FOUNDATION_VERSION: String = "1.0.0"
-    const val LBC_ACCESSIBILITY_VERSION: String = "1.4.0"
-    const val LBC_THEME_VERSION: String = "1.0.0"
-    const val MATERIAL_COLOR_UTILITIES_VERSION: String = "1.0.0"
+    const val LBC_CORE_VERSION: String = "1.1.0"
+    const val LBC_FOUNDATION_VERSION: String = "1.1.0"
+    const val LBC_ACCESSIBILITY_VERSION: String = "1.5.0"
+    const val LBC_THEME_VERSION: String = "1.1.0"
+    const val MATERIAL_COLOR_UTILITIES_VERSION: String = "1.1.0"
 }
