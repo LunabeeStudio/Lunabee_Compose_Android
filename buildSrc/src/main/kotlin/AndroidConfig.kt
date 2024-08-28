608380a4ab2394bd4155bfa8efd32ad41f5a33e6
Hugo Bernardi <hugo.bernardi@lunabee.com>
2024-08-28 18:35:36 +0200

    Fix version

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -37,10 +37,10 @@ object AndroidConfig {
     // ⚠️ Match module name in UPPER_CASE ('-' -> '_')
     const val LBCCORE_VERSION: String = "1.4.0"
     const val LBCFOUNDATION_VERSION: String = "1.3.0"
-    const val LBCANDROIDTEST_VERSION: String = "1.2.0"
+    const val LBCANDROIDTEST_VERSION: String = "1.3.0"
     const val LBCACCESSIBILITY_VERSION: String = "1.7.0"
     const val LBCTHEME_VERSION: String = "1.3.0"
-    const val MATERIAL_COLOR_UTILITIES_VERSION: String = "1.2.0"
+    const val MATERIAL_COLOR_UTILITIES_VERSION: String = "1.3.0"
     const val LBCHAPTIC_VERSION: String = "1.1.0"
     const val LBCUIFIELD_VERSION: String = "1.0.2"
     const val LBCIMAGE_VERSION: String = "1.0.1"
