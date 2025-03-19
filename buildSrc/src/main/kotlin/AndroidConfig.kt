ad720cc8f09ed7b0c1dcc5e5ce18cf6cc1719e48
rlatapy-luna <remi.latapy@lunabee.com>
2025-03-19 23:30:59 +0100

    bump lbcpresenter

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -45,7 +45,7 @@ object AndroidConfig {
     const val LBCUIFIELD_VERSION: String = "1.6.1"
     const val LBCIMAGE_VERSION: String = "1.4.0"
     const val LBCGLANCE_VERSION: String = "1.3.0"
-    const val LBCPRESENTER_VERSION: String = "1.3.0"
+    const val LBCPRESENTER_VERSION: String = "1.4.0"
     const val LBCPRESENTER_KOIN_VERSION: String = LBCPRESENTER_VERSION
 
     val JDK_VERSION: JavaVersion = JavaVersion.VERSION_17
