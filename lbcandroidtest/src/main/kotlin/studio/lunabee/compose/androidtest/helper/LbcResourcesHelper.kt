3eea08a0287553fa1f3551107987d3e5c087f338
Publisher Lunabee <publisher@lunabee.com>
2025-09-08 09:51:32 +0200

    Detekt

--- lbcandroidtest/src/main/kotlin/studio/lunabee/compose/androidtest/helper/LbcResourcesHelper.kt
+++ lbcandroidtest/src/main/kotlin/studio/lunabee/compose/androidtest/helper/LbcResourcesHelper.kt
@@ -100,8 +100,12 @@ object LbcResourcesHelper {
                 val newFile = File(context.cacheDir, folderResource.pathResourceName)
                 newFile.mkdirs()
             } else {
-                javaClass.classLoader!!.getResourceAsStream(folderResource.pathResourceName).use { inputStream ->
-                    File(context.cacheDir, folderResource.pathResourceName).outputStream().use { outputStream ->
+                javaClass.classLoader!!.getResourceAsStream(folderResource.pathResourceName).use {
+                    inputStream
+                    ->
+                    File(context.cacheDir, folderResource.pathResourceName).outputStream().use {
+                        outputStream
+                        ->
                         inputStream.copyTo(outputStream)
                     }
                 }
