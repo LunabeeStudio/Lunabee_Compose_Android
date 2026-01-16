5aa14b71d863122d062838ccf7b654e3adbf4588
rlatapy-luna <remi.latapy@lunabee.com>
2026-01-16 16:06:40 +0100

    Fix next version is 4.9.0

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -65,13 +65,13 @@ object AndroidConfig {
     const val LBCPRESENTER_VERSION: String = "1.10.0"
     const val LBCPRESENTER_KOIN_VERSION: String = LBCPRESENTER_VERSION
     const val LBCROBOLECTRICTEST_VERSION: String = "1.1.0"
-    const val LBLOGGER_KERMIT_VERSION: String = "4.10.0"
-    const val LBCORE_VERSION: String = "4.10.0"
-    const val LBEXTENSIONS_VERSION: String = "4.10.0"
-    const val LBTEST_VERSION: String = "4.10.0"
-    const val LBKTOR_VERSION: String = "4.10.0"
-    const val LBLOADING_VERSION: String = "4.10.0"
-    const val LBPLATFORM_VERSION: String = "4.10.0"
+    const val LBLOGGER_KERMIT_VERSION: String = "4.9.0"
+    const val LBCORE_VERSION: String = "4.9.0"
+    const val LBEXTENSIONS_VERSION: String = "4.9.0"
+    const val LBTEST_VERSION: String = "4.9.0"
+    const val LBKTOR_VERSION: String = "4.9.0"
+    const val LBLOADING_VERSION: String = "4.9.0"
+    const val LBPLATFORM_VERSION: String = "4.9.0"
 
     val JDK_VERSION: JavaVersion = JavaVersion.VERSION_21
     val JVM_TARGET: JvmTarget = JvmTarget.JVM_21
