b0aad87eebcbbbe310a8dfc9919a021b37b783a8
rlatapy-luna <remi.latapy@lunabee.com>
2026-01-20 12:29:55 +0100

    wip rework BoM builder

--- common/lbplatform/build.gradle.kts
+++ common/lbplatform/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.internal.catalog.DelegatingProjectDependency
+
 /*
  * Copyright (c) 2026 Lunabee Studio
  *
@@ -20,57 +22,34 @@ plugins {
 }
 
 val rootProject: String = "studio.lunabee"
-val versionName: String = AndroidConfig.commonVersionName()
+description = "Lunabee libraries bill of materials"
+version = AndroidConfig.commonVersionName()
 
 dependencies {
     constraints {
-        addKmpModule(projects.lbcore.name, Target.Jvm)
-        addModule(projects.lbcoreAndroid.name)
-        addModule(projects.lbcoreCompose.name)
-        addModule(projects.lbextensions.name)
-        addModule(projects.lbextensionsAndroid.name)
-        addKmpModule(projects.lbktorCore.name, Target.Jvm)
-        addKmpModule(projects.lbktorJson.name, Target.Jvm)
-        addKmpModule(projects.lbktorKermit.name, Target.Jvm)
-        addModule(projects.lbloadingCompose.name)
-        addModule(projects.lbloadingChecks.name)
-        addKmpModule(projects.lbloadingCore.name, Target.Jvm)
-        addModule(projects.lbloadingHilt.name)
-        addModule(projects.lbloadingKoin.name)
-        addKmpModule(projects.lbloggerKermit.name, Target.Jvm)
-        addKmpModule(projects.lbloggerKermitCrashlytics.name, Target.Android)
-        addModule(projects.lbtest.name)
+        addModule(projects.lbcore)
+        addModule(projects.lbcoreAndroid)
+        addModule(projects.lbcoreCompose)
+
+        addModule(projects.lbktorCore)
+        addModule(projects.lbktorJson)
+        addModule(projects.lbktorKermit)
+
+        addModule(projects.lbloadingCompose)
+        addModule(projects.lbloadingChecks)
+        addModule(projects.lbloadingCore)
+        addModule(projects.lbloadingHilt)
+        addModule(projects.lbloadingKoin)
+
+        addModule(projects.lbloggerKermit)
+        addModule(projects.lbloggerKermitCrashlytics)
+
+        addModule(projects.lbextensions)
+        addModule(projects.lbextensionsAndroid)
+        addModule(projects.lbtest)
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("lunabeeBom") {
-            from(components["javaPlatform"])
-
-            groupId = "studio.lunabee"
-            version = versionName
-            artifactId = "lunabee-bom"
-        }
-    }
-}
-
-fun DependencyConstraintHandlerScope.addModule(module: String) {
-    api("$rootProject:${module.substringAfterLast(':')}:$versionName")
-}
-
-fun DependencyConstraintHandlerScope.addKmpModule(module: String, target: Target) {
-    api("$rootProject:${module.substringAfterLast(':')}:$versionName")
-    api("$rootProject:${module.substringAfterLast(':')}-iosarm64:$versionName")
-    api("$rootProject:${module.substringAfterLast(':')}-iossimulatorarm64:$versionName")
-    api("$rootProject:${module.substringAfterLast(':')}-iosx64:$versionName")
-    when (target) {
-        Target.Jvm -> api("$rootProject:${module.substringAfterLast(':')}-jvm:$versionName")
-        Target.Android -> api("$rootProject:${module.substringAfterLast(':')}-android:$versionName")
-    }
-}
-
-enum class Target {
-    Jvm,
-    Android,
+fun DependencyConstraintHandlerScope.addModule(module: DelegatingProjectDependency) {
+    api(project(":${module.name.substringAfterLast(':')}"))
 }
