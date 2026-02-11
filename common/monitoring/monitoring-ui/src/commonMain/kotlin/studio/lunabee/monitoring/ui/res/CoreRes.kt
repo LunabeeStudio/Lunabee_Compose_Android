8525df58ab1bb02d1130ddb74473a35b5644b9ae
rlatapy-luna <remi.latapy@lunabee.com>
2026-02-11 13:21:49 +0100

    remove import

--- common/monitoring/monitoring-ui/src/commonMain/kotlin/studio/lunabee/monitoring/ui/res/CoreRes.kt
+++ common/monitoring/monitoring-ui/src/commonMain/kotlin/studio/lunabee/monitoring/ui/res/CoreRes.kt
@@ -16,7 +16,5 @@
 
 package studio.lunabee.monitoring.ui.res
 
-import studio.lunabee.monitoring.ui.res.Res
-
 internal typealias CoreString = Res.string
 internal typealias CoreDrawable = Res.drawable
